68826cd82c204ab0a695a53ec20aa19043bff008
Tanmay2510 <comparisionthing@gmail.com>
2022-10-01 01:05:07 +0530

    Responsive

--- src/components/Find/Find.jsx
+++ src/components/Find/Find.jsx
@@ -4,51 +4,63 @@ import axios from 'axios';
 import Card from "../Cards/Card.jsx"
 function Find() {
   const [name, setname] = useState("");
+  const [date,setdate] =useState(null)
   const [isdata,setisdata] = useState(false);
   const [reald,setreald] = useState({});
   const [realdstatus,setrealdstatus] = useState(false);
-     // useEffect(() => {
-  //   axios.get("http://localhost:5000/get").then((response) => {
-  //    console.log(response);
-  //   })
-  // }, [])
-  const handle = (e) => {
+    
+  const handl = (e) => {
     e.preventDefault();
+    console.log(name)
+
     try {
-      axios.post("http://localhost:5000/get", {
+      axios.post("http://localhost:5000/getusers", {
         name
       }).then((x) => {
         setisdata(!isdata);
         if(x.data.data.length === 0){
           setreald([]);
-          console.log("HERe");
           setrealdstatus(true);
         }else if(x.data.data.length>0){
-      setreald(x.data.data[0].orders);
+      setreald(x.data.data[0].orders);-
       setrealdstatus(false);
     }
       })
     } catch (error) {
       console.log(error)
     }
-  }    
+  }  
+  const hand = (e) => {
+    e.preventDefault();
+    try {
+      axios.post("http://localhost:5000/getdate", {
+        date
+      }).then((x) => {
+        console.log(x);
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }     
+  console.log(name)
   return (
     <div>
       <div className='box'>
       <h1>Hey User</h1>
       <hr></hr>
       <p>You can search for orders of owner by their first name or order date</p>
-      <form onSubmit={handle}>
+      <form onSubmit={handl}>
         <input type="text"
           onChange={(e) => {
             setname(e.target.value);
           }}
-          placeholder='Enter first name of spotlight users'></input>
-        <button>GO</button>
+          placeholder='Enter first name of spotlight users'
+          value={name}></input>
+        <button type="submit">GO</button>
       </form>
-      <form >
-        <input type="date" placeholder='Enter date'></input>
-        <button>GO</button>
+      <form onSubmit={hand}>
+        <input type="date" onChange={(e)=>{setdate(e.target.value)}}></input>
+        <button type="submit">GO</button>
       </form>
       
     </div>
@@ -57,6 +69,8 @@ function Find() {
       : 
         
           isdata  ?
+          <div>
+          <p>Ordered by {name}</p>
            <div className="boxx"> 
            {
             reald.map(el =>{
@@ -70,6 +84,7 @@ function Find() {
     
             })
           }
+          </div>
          
           </div> : null}
      
