78478c3e7fe27789d6098aa1145c8d2915986228
Tanmay2510 <comparisionthing@gmail.com>
2022-10-05 22:21:27 +0530

    Completely done

--- src/components/Find/Find.jsx
+++ src/components/Find/Find.jsx
@@ -1,5 +1,5 @@
 import "./Find.css"
-import react, { useState , useEffect } from "react";
+import react, { useState, useEffect, useRef } from "react";
 import axios from 'axios';
 import Card from "../Cards/Card.jsx"
 
@@ -10,12 +10,12 @@ function Find() {
   const [isdatacal, setisdatacal] = useState(true)
   const [reald, setreald] = useState({});
   const [realdd, setrealdd] = useState({});
-  const [iscalval, setiscalval] = useState(false);  
+  const [iscalval, setiscalval] = useState(false);
   const [realdstatus, setrealdstatus] = useState(false);
-  const [thea,setthea] = useState([]);
-  const [distp,setdisp] = useState(false);
-  const [isf,setisf] = useState(false);
-
+  const [thea, setthea] = useState([]);
+  const [distp, setdisp] = useState(false);
+  const [isf, setisf] = useState(false);
+  const wrapperref = useRef(null)
   const handl = (e) => {
     try {
       axios.get("http://localhost:5000/getusers/" + name).then((x) => {
@@ -31,6 +31,7 @@ function Find() {
     } catch (error) {
       console.log(error)
     }
+    setdisp(false)
     e.preventDefault();
   }
   let a = [];
@@ -42,16 +43,16 @@ function Find() {
           setiscalval(true)
         } else if (x.data.dat.length > 0) {
           x.data.dat.map(el => {
-            el.orders.map(finaldate=>{
-               if(finaldate.date.substring(0,10) == date){
+            el.orders.map(finaldate => {
+              if (finaldate.date.substring(0, 10) == date) {
                 console.log(el.firstName)
                 const ne = {
-                  name:el.firstName,
-                  arr:finaldate
+                  name: el.firstName,
+                  arr: finaldate
                 }
                 a.push(ne)
                 setrealdd(a)
-               }
+              }
             })
             setiscalval(false)
           })
@@ -63,17 +64,29 @@ function Find() {
     }
   }
   useEffect(() => {
-      try {
-          axios.get("http://localhost:5000/search").then(x=>{
-            setthea(x.data.data);
-          })
-      } catch (error) {
-        console.log(error)
-      }
-  },[])
+    try {
+      axios.get("http://localhost:5000/search").then(x => {
+        setthea(x.data.data);
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+  useEffect(() => {
+    document.addEventListener("mousedown", handleclickoutside)
+    return () => {
+      document.removeEventListener("mousedown", handleclickoutside)
+    }
+  }, [])
+  const handleclickoutside = event => {
+    const { current: wrap } = wrapperref;
+    if (wrap && !wrap.contains(event.target)) {
+      setdisp(false)
+    }
+  }
   let newname = [];
-  thea.map(l=>{
-    if(l.toLowerCase().indexOf(name.toLowerCase())>-1){
+  thea.map(l => {
+    if (l.toLowerCase().indexOf(name.toLowerCase()) > -1) {
       newname.push(l)
     }
   })
@@ -84,28 +97,42 @@ function Find() {
         <hr></hr>
         <p>You can search for orders of owner by their first name or order date</p>
         <form onSubmit={handl}>
-        <div>
-          <input type="text"
-            onFocus={()=>setdisp(true)}
-            // onBlur={()=>setdisp(false)}
-            onChange={(e) => {
-              setname(e.target.value);
-            }}
-            placeholder='Enter first name of spotlight users'
-            value={name}/>
-          {
-            distp &&
-              newname.map(thename=>{
-                return(
-                  <div onClick={()=>setname(thename)} >{thename}</div>
-                )
-              })
-          }
-        </div>
+          <div ref={wrapperref} className="flexContainer flex-column pos-rel">
+            <input type="text"
+              onClick={() => setdisp(true)}
+
+              onChange={(e) => {
+                setdisp(true)
+                setname(e.target.value);
+              }}
+              placeholder='Enter first name of spotlight users'
+              value={name} />
+            <button type="submit" onClick={() => setdisp(false)}>Go</button>
+            {
+              distp &&
+              <div className="autoContainer">
+                {newname.map(thename => {
+                  return (
+                    <div
+                      tabIndex="0"
+                      className="option" onClick={() => {
+                        setname(thename)
+                      }} >{thename}</div>
+                  )
+
+                })}
+              </div>
+
+            }
+          </div>
+
         </form>
         <form onSubmit={hand}>
-          <input type="date" onChange={(e) => { setdate(e.target.value) }}
+          <input
+            style={{ marginTop: "90px" }}
+            type="date" onChange={(e) => { setdate(e.target.value) }}
             value={date}></input>
+          <button type="submit" >Go</button>
         </form>
       </div>
       {realdstatus ? <p>No user found(by name search)</p> : isdata ?
@@ -135,14 +162,14 @@ function Find() {
               <div className="boxx" >
                 {
                   realdd.map((dateobj) => {
-                   return(
-                    <Card
-                    listid={dateobj.arr.list}
-                    amount={dateobj.arr.amount}
-                    dat={dateobj.arr.date}
-                    name={dateobj.name}
-                  />
-                   )
+                    return (
+                      <Card
+                        listid={dateobj.arr.list}
+                        amount={dateobj.arr.amount}
+                        dat={dateobj.arr.date}
+                        name={dateobj.name}
+                      />
+                    )
                   })
                 }
               </div>
