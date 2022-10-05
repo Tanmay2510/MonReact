f2a2397bc865739d1a62f71d9e4f6ec838eadd0b
Tanmay2510 <comparisionthing@gmail.com>
2022-10-05 17:07:55 +0530

    Suggestion style need

--- src/components/Find/Find.jsx
+++ src/components/Find/Find.jsx
@@ -1,7 +1,8 @@
 import "./Find.css"
-import react, { useState } from "react";
+import react, { useState , useEffect } from "react";
 import axios from 'axios';
 import Card from "../Cards/Card.jsx"
+
 function Find() {
   const [name, setname] = useState("");
   const [date, setdate] = useState(null)
@@ -9,8 +10,12 @@ function Find() {
   const [isdatacal, setisdatacal] = useState(true)
   const [reald, setreald] = useState({});
   const [realdd, setrealdd] = useState({});
-  const [iscalval, setiscalval] = useState(false);  //for date null or not
+  const [iscalval, setiscalval] = useState(false);  
   const [realdstatus, setrealdstatus] = useState(false);
+  const [thea,setthea] = useState([]);
+  const [distp,setdisp] = useState(false);
+  const [isf,setisf] = useState(false);
+
   const handl = (e) => {
     try {
       axios.get("http://localhost:5000/getusers/" + name).then((x) => {
@@ -29,13 +34,12 @@ function Find() {
     e.preventDefault();
   }
   let a = [];
-  let b = [];
   const hand = (e) => {
     e.preventDefault();
     try {
       axios.get("http://localhost:5000/getdate/" + date).then((x) => {
         if (x.data.dat.length === 0) {
-          // setiscalval(true)
+          setiscalval(true)
         } else if (x.data.dat.length > 0) {
           x.data.dat.map(el => {
             el.orders.map(finaldate=>{
@@ -58,7 +62,21 @@ function Find() {
       console.log(error)
     }
   }
-  console.log(realdd)
+  useEffect(() => {
+      try {
+          axios.get("http://localhost:5000/search").then(x=>{
+            setthea(x.data.data);
+          })
+      } catch (error) {
+        console.log(error)
+      }
+  },[])
+  let newname = [];
+  thea.map(l=>{
+    if(l.toLowerCase().indexOf(name.toLowerCase())>-1){
+      newname.push(l)
+    }
+  })
   return (
     <div>
       <div className='box'>
@@ -66,18 +84,28 @@ function Find() {
         <hr></hr>
         <p>You can search for orders of owner by their first name or order date</p>
         <form onSubmit={handl}>
+        <div>
           <input type="text"
+            onFocus={()=>setdisp(true)}
+            // onBlur={()=>setdisp(false)}
             onChange={(e) => {
               setname(e.target.value);
             }}
             placeholder='Enter first name of spotlight users'
-            value={name}></input>
-          <button type="submit">GO</button>
+            value={name}/>
+          {
+            distp &&
+              newname.map(thename=>{
+                return(
+                  <div onClick={()=>setname(thename)} >{thename}</div>
+                )
+              })
+          }
+        </div>
         </form>
         <form onSubmit={hand}>
           <input type="date" onChange={(e) => { setdate(e.target.value) }}
             value={date}></input>
-          <button type="submit">GO</button>
         </form>
       </div>
       {realdstatus ? <p>No user found(by name search)</p> : isdata ?
